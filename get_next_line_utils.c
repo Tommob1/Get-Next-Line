d625364007017bfd1946b03c9249b6cfd6ab63b2
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-04-04 17:20:20 +0930

    Added norminette corrections to get_next_line_utils.c file

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tpawson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/03 16:26:49 by btomlins          #+#    #+#             */
-/*   Updated: 2023/04/04 15:41:40 by btomlins         ###   ########.fr       */
+/*   Created: 2023/03/23 10:07:41 by tpawson           #+#    #+#             */
+/*   Updated: 2023/04/04 15:41:43 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 size_t	ft_strlen(const char *s)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -23,84 +23,75 @@ size_t	ft_strlen(const char *s)
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s || ((*s ++ '\0') && (c == '\0')))
+	while (*s || ((*s == '\0') && (c == '\0')))
 	{
 		if (((char)*s) == ((char)c))
-			return	((char *)s);
+			return ((char *)s);
 		s++;
 	}
 	return (0);
 }
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	size_t	a;
-	size_t	b;
-	size_t	strlen;
+	size_t	i;
+	size_t	k;
+	char	*sub;
 
-	strlen = ft_strlen(s);
-	if (start > strlen)
-		return (ft_strdup(""));
-	if (len > strlen - start)
-		len = strlen - start;
-	a = 0;
-	b = 0;
-	ptr = (char *)malloc(len + 1);
-	if (!ptr)
+	i = 0;
+	k = 0;
+	sub = (char *)malloc((sizeof(char)) * (len + 1));
+	if (!sub)
 		return (0);
-	while (s[a])
+	while (s[i])
 	{
-		if (b < len && a >= start)
-		{
-			ptr[b++] = s[a];
-		}
-		a++;
+		if (k < len && i >= start)
+			sub[k++] = s[i];
+		i++;
 	}
-	ptr[b] = '\0';
-	return (ptr);
+	sub[k] = '\0';
+	return (sub);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
+	char	*tlen;
+	size_t	i;
+	size_t	k;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = NULL;
-	if (!s1 || !s2)
+	i = 0;
+	k = 0;
+	tlen = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!tlen)
 		return (0);
-	else
+	while (s1[i] != '\0')
 	{
-		str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-		if (!str)
-			return (0);
-		ft_strlcpy(str, s1, s1_len + 1);
-		ft_strlcpy(str + (s1_len), s2, s2_len + 1);
+		tlen[i] = s1[i];
+		i++;
 	}
-	return (str);
+	while (s2[k] != '\0')
+	{
+		tlen[i + k] = s2[k];
+		k++;
+	}
+	tlen[i + k] = '\0';
+	return (tlen);
 }
 
 char	*ft_strdup(const char *s1)
 {
-	char	*dest;
-	int		s1_len;
-	int		i;
+	char		*s2;
+	size_t		i;
 
-	s1_len = 0;
-	while (s1[s1_len])
-		s1_len++;
-	dest = (char *)malloc(sizeof(char) * (s1_len + 1));
-	if (!dest)
-		return (NULL);
 	i = 0;
-	while (i < s1_len)
+	s2 = malloc(ft_strlen(s1) + 1);
+	if (!s1)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		dest[i] = s1[i];
+		s2[i] = s1[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	s2[i] = '\0';
+	return (s2);
 }
