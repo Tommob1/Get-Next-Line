8cc5a3041a8f2816fc30b2216ae98430f43d27d7
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-04-04 16:12:52 +0930

    Added includes to header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,23 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:27:22 by btomlins          #+#    #+#             */
-/*   Updated: 2023/04/03 16:27:27 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/04/04 15:00:26 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEW_LINE_H
+#define BUFFER_SIZE 8
+#define FD_SIZE 4096
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
