6aee5ec3f74eac29e62d0718418fbe32660cc5aa
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-04-06 16:30:54 +0930

    Added get_next_line function and started read_file function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:25:41 by btomlins          #+#    #+#             */
-/*   Updated: 2023/04/06 13:55:51 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/04/06 16:29:26 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,42 @@ static void	free_ptr(char **ptr)
 	*ptr = NULL;
 }
 
+static char	*read_file(char *temp, int fd, char *buffer)
+{
+	size_t i;
+
+	i = 1;
+	while (i && !ft_strchr (temp,  '\n'))
+	{
+
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	char		*buffer;
-	char		*out;
-	static char	*buffcpy[OPEN_LIMIT];
+	static char 	*temp[OPEN_MAX];
+	char			*buffer;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > OPEN_LIMIT)
+	if (fd == -1 || BUFFER_SIZE < 1)
 		return (0);
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!temp[fd])
+		temp[fd] = ft_strdup("");
+	if (!temp[fd])
+		return (0);
+	buffer = malloc(sizeof(*buffer) * (BUFFER_SIZE + 1));
 	if (!buffer)
+	{
+		free (temp[fd]);
 		return (0);
-	if (!buffcpy[fd])
-		buffcpy[fd] = ft_strdup("");
+	}
+	temp[fd] = read_file(temp[fd], fd, buffer);
+	if (!temp[fd])
+		return (0);
+	if (!*temp[fd])
+	{
+		free(temp[fd]);
+		temp[fd] = 0;
+		return (0);
+	}
+	return (next(&temp[fd]));
 }
