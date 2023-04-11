2e45f609072a6782f09b7a06260db482ab676b3d
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-04-11 11:54:21 +0930

    Completed read_file function using strjoin_free helper function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:25:41 by btomlins          #+#    #+#             */
-/*   Updated: 2023/04/06 16:32:32 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/04/11 11:52:49 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,38 @@ static void	free_ptr(char **ptr)
 	*ptr = NULL;
 }
 
+static char	*next(char **temp)
+{
+	char *s;
+	char *ptr;
+
+	ptr = *temp;
+}
+
 static char	*read_file(char *temp, int fd, char *buffer)
 {
-	size_t	i;
+	ssize_t	i;
 
 	i = 1;
 	while (i && !ft_strchr (temp, '\n'))
 	{
+		r = read (fd, bufffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free (buffer);
+			free (temp);
+			return (0);
+		}
+		buffer[r] = 0;
+		temp = ft_strjoin_free(temp, buffer);
+		if (!temp)
+		{
+			free (buf);
+			return (0);
+		}
 	}
+	free (buf);
+	return (temp);
 }
 
 char	*get_next_line(int fd)
